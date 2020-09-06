03a354775f640fce4f69fa5888fbbc3fb7e40769
Georgiy Demo <demenchuk.george@protonmail.com>
2020-09-06 14:53:17 +0300

    Тестинг

--- maker.py
+++ maker.py
@@ -1,87 +1,93 @@
 from PIL import ImageDraw, ImageFont, Image, ImageOps
 
-#############################
-LOGO_SOURCE = "./logo.jpg"
-INPUT_IMG = "./image.jpg"
-OUTPUT_IMG = "./result.jpg"
-TTF_SOURCE = "./times.ttf"
-IMG_TEXT_FIRST = "ОТЕЛЬ НА БАЛИ"
-IMG_TEXT_SECOND = "USD 2.500.000"
-COUNTRY_FLAG = "ECU"
-#############################
 
+class ImageGenerator:
+    TTF_SOURCE = "./times.ttf"
+    LOGO_SOURCE = "./logo.jpg"
 
-def crop_img(img):
-    """Обрезка изображения по квадрату"""
-    crop_width, crop_height = img.size
-    value = crop_width if crop_width < crop_height else crop_height
-    area = (0, 0, value, value)
-    img = img.crop(area)
-    return img
+    def __init__(self, input_img, output_img, first_text, second_text, country="ecu"):
 
+        self.input_img = input_img
+        self.output_img = output_img
+        self.first_text = first_text
+        self.second_text = second_text
+        self.country = country
 
-def resizer(img, basewidth):
-    """Ресайз фото фото"""
-    width, height = img.size
-    wpercent = basewidth / float(width)
-    hsize = int((float(height) * float(wpercent)))
-    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-    return img
+        self.processing()
 
+    def crop_img(self, img):
+        """Обрезка изображения по квадрату"""
+        crop_width, crop_height = img.size
+        value = crop_width if crop_width < crop_height else crop_height
+        area = (0, 0, value, value)
+        img = img.crop(area)
+        return img
 
-# Открываем основное изображение
-photo = Image.open(INPUT_IMG)
-width, height = photo.size
-# Открываем флаг
-national_flag = Image.open("./flags/" + COUNTRY_FLAG.lower() + ".png")
-# Открываем лого
-company_logo = Image.open(LOGO_SOURCE)
+    def resizer(self, img, basewidth):
+        """Ресайз фото"""
+        width, height = img.size
+        wpercent = basewidth / float(width)
+        hsize = int((float(height) * float(wpercent)))
+        img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+        return img
 
+    def processing(self):
+        # Открываем основное изображение
+        photo = Image.open(self.input_img)
+        width, height = photo.size
+        # Открываем флаг
+        national_flag = Image.open("./flags/" + self.country.lower() + ".png")
+        # Открываем лого
+        company_logo = Image.open(self.LOGO_SOURCE)
 
-# Предобработка основого фото
-if width != height:
-    photo = crop_img(photo)
-    width, height = photo.size
-# Если больше - уменьшаем
-if width > 1080:
-    photo = resizer(photo, 1080)
-# Если меньше - увеличиваем
-else:
-    photo = photo.resize((1080, 1080), resample=Image.BOX)
-width, height = photo.size
+        # Предобработка основого фото
+        if width != height:
+            photo = self.crop_img(photo)
+            width, height = photo.size
+        # Если больше - уменьшаем
+        if width > 1080:
+            photo = self.resizer(photo, 1080)
+        # Если меньше - увеличиваем
+        else:
+            photo = photo.resize((1080, 1080), resample=Image.BOX)
+        width, height = photo.size
 
-# Предобработка флага
-national_flag = resizer(national_flag, 190)
-national_flag = ImageOps.expand(national_flag, border=3, fill="black")
+        # Предобработка флага
+        national_flag = self.resizer(national_flag, 190)
+        national_flag = ImageOps.expand(national_flag, border=3, fill="black")
 
-# Создаем пустое изображение
-background = Image.new("RGB", (width, height), (255, 255, 255))
-draw = ImageDraw.Draw(background)
-# Вставляем основное фото
-background.paste(photo)
+        # Создаем пустое изображение
+        background = Image.new("RGB", (width, height), (255, 255, 255))
+        draw = ImageDraw.Draw(background)
+        # Вставляем основное фото
+        background.paste(photo)
 
-# Добавляем прямоугольник вниз
-draw.rectangle(((0, 898), (1080, 1080)), fill="white")
+        # Добавляем прямоугольник вниз
+        draw.rectangle(((0, 898), (1080, 1080)), fill="white")
 
-# Добавляем основной текст
-fontsize = 50
-font = ImageFont.truetype(TTF_SOURCE, fontsize)
-w, h = draw.textsize(IMG_TEXT_FIRST, font)
-left = (width - w) / 2
-draw.text((left, 925), IMG_TEXT_FIRST, font=font, fill=(6, 26, 113), align="center")
+        # Добавляем основной текст
+        fontsize = 50
+        font = ImageFont.truetype(self.TTF_SOURCE, fontsize)
+        w, h = draw.textsize(self.first_text, font)
+        left = (width - w) / 2
+        draw.text(
+            (left, 925), self.first_text, font=font, fill=(6, 26, 113), align="center"
+        )
 
-# Добавляем текст с ценой
-fontsize = 45
-font = ImageFont.truetype(TTF_SOURCE, fontsize)
-w, h = draw.textsize(IMG_TEXT_SECOND, font)
-left = (width - w) / 2
-draw.text((left, 1000), IMG_TEXT_SECOND, font=font, fill=(6, 26, 113), align="center")
+        # Добавляем текст с ценой
+        fontsize = 45
+        font = ImageFont.truetype(self.TTF_SOURCE, fontsize)
+        w, h = draw.textsize(self.second_text, font)
+        left = (width - w) / 2
+        draw.text(
+            (left, 1000), self.second_text, font=font, fill=(6, 26, 113), align="center"
+        )
 
-# Добавляем флаг
-background.paste(national_flag, (25, 925))
+        # Добавляем флаг
+        background.paste(national_flag, (25, 925))
 
-# Добавляем лого
-background.paste(company_logo, (850, 925))
+        # Добавляем лого
+        background.paste(company_logo, (850, 925))
 
-# Сохраняем изображение
-background.save(OUTPUT_IMG)
+        # Сохраняем изображение
+        background.save(self.output_img)
