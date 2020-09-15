a10fdffb400918350f486711e5194ed4e902d9b1
Georgiy Demo <demenchuk.george@protonmail.com>
2020-09-15 13:38:50 +0300

    Каркас для telegram

--- telegram/code/app.py
+++ telegram/code/app.py
@@ -10,6 +10,8 @@ class TelegramCli:
 
     def __init__(self, token, proxy):
 
+
+        self.buffer_dict = {}
         self.update_id = None
 
         if proxy is not None and proxy != "":
@@ -34,13 +36,45 @@ class TelegramCli:
 
         for update in self.bot.get_updates(offset=self.update_id, timeout=10):
             self.update_id = update.update_id + 1
+
+            user_id = update.message.chat.id
             if update.message.text is None:
                 user_msg = ""
             else:
                 user_msg = update.message.text
 
+            #Если это начало работы с ботом
             if user_msg == "/start":
-                update.message.reply_text("MEOW")
+                update.message.reply_text("Привет, пришли мне изображение")
+
+            #Если пользователь прислал файл
+            elif update.message.document is not None:
+                file_data = update.message.document
+                file_name = file_data["file_name"]
+
+                new_file = self.bot.getFile(file_data["file_id"])
+                file_source = "./images/{}".format(file_name)
+                new_file.download(file_source)
+
+                self.buffer_dict[user_id] = file_source
+                update.message.reply_text("СПАСИБО ЗА ФАЙЛ, Я ЕГО ЗАГРУЗИЛ\nТеперь пришли параметры для формирования изображения")
+
+            #Если пользователь отправил сообщение и он есть в buffer_dict, то это параматры для файла
+            elif user_msg != "" and user_id in self.buffer_dict:
+                
+                file_args = list(map(lambda x: x.replace("\"", ""),[value for value in user_msg.split(" ")]))
+                if len(file_args) == 3:
+                    title, subtitle, flag = file_args
+                    with open(self.buffer_dict[user_id]) as file:
+                        r = requests.post("http://flask:5000/create_image", data={"title" : title, "subtitle" : subtitle, "flag" : flag}, files={"image" : file}).json()
+                    
+    
+                    update.message.reply_text("Ваш результат:")
+                    
+                    del self.buffer_dict[user_id]
+                
+                else:
+                    update.message.reply_text("Некорректный ввод параметров")
 
 def main():
 
--- testing/check.py
+++ testing/check.py
@@ -5,14 +5,13 @@ text1 = "MEOW"
 text2 = "2000 $"
 flag = "hti"
 
-r = None
 with open ("./image.jpg", "rb") as file:
     r = requests.post("http://127.0.0.1:5000/create_image",data={"title" : text1, "subtitle" : text2, "flag" : flag}, files={"image" : file}).json()
 
 if not r["exception"]:
-    imgdata = base64.b64decode(r["result"])
+    img_data = base64.b64decode(r["result"])
     with open('./result.jpg', 'wb') as f:
-        f.write(imgdata)
+        f.write(img_data)
 
 # from io import BytesIO
 # bio = BytesIO()
